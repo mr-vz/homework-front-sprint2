bcecccda896cb71c2db1f655fa8368f4b0c0bce0
mr-vz <vladimirzaharov94@gmail.com>
2024-09-07 00:18:32 +0700

    hw04

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -22,7 +22,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         //         ? ...
         //         : xType === 'red'
         //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' +  s.red : xType === 'secondary' ? ' ' +  s.secondary : ' ' + s.default) + (className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
