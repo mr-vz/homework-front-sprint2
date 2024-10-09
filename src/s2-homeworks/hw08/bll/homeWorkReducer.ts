58199ad665a5a275bb4b559b5f03265a76fa26c7
mr-vz <vladimirzaharov94@gmail.com>
2024-10-09 16:17:43 +0700

    hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            //return state
+            return action.payload === 'up'
+                ? state.sort((a, b) => (a.name > b.name) ? 1 : -1)
+                : state.sort((a, b) => (a.name < b.name) ? 1 : -1)
         }
         case 'check': {
-
-            return state // need to fix
+             // need to fix
+            return state.filter(p => p.age > action.payload)
         }
         default:
             return state
